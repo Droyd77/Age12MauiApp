5d997e893cab7c3fe8009ff75ee17971bc453d58
david <david@DROYD77-DESKTOP>
2025-10-21 16:12:45 -0300

    Adicionar arquivos de projeto.

---
+++ Age12MauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Age12MauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Age12MauiApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Age12MauiApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
